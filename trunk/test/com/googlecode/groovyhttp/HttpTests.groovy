f6cf99575e5781741bdfcf7366f7b7dd94ca9626
mingfai.ma <mingfai.ma@31e05524-220e-11de-8f7e-2b794b28c5cd>
2009-04-06 06:03:02 +0000

    import

---
+++ trunk/test/com/googlecode/groovyhttp/HttpTests.groovy
@@ -0,0 +1,56 @@
+package com.googlecode.groovyhttp
+
+import org.apache.http.client.methods.HttpGet
+import org.apache.http.HttpEntity
+import net.htmlparser.jericho.HTMLElementName
+
+public class HttpTests extends GroovyTestCase {
+  static TEST_URL = "http://code.google.com/p/groovy-http/"
+
+  void testGetReturnType() {
+    assertEquals Http.class, new Http()?.getClass()
+    assertEquals Http.class, new Http().get(TEST_URL)?.getClass()
+    new Http().get(TEST_URL) {
+      delegate.assertTrue(it instanceof HttpGet)
+    }
+    new Http().get(TEST_URL) {get ->
+      delegate.assertTrue(get instanceof HttpGet)
+    }
+    new Http().get(TEST_URL) {get, entity ->
+      delegate.assertTrue(get instanceof HttpGet)
+      delegate.assertTrue(entity instanceof HttpEntity)
+    }
+    new Http().get(TEST_URL) {get, entity, client ->
+      delegate.assertTrue(get instanceof HttpGet)
+      delegate.assertTrue(entity instanceof HttpEntity)
+      delegate.assertTrue(client instanceof Http)
+    }
+    assertTrue new Http().get(TEST_URL) {it} instanceof HttpGet
+    assertTrue new Http().get(TEST_URL) {g, e, c -> c} instanceof Http
+  }
+
+  void testGetSize() {
+    assertTrue new Http().get(TEST_URL).size() > 8000
+  }
+
+  void testPost() {
+    println new Http().post(TEST_URL, [[test: 'test']]).text
+  }
+
+  void testGetForm() {
+    def form = new Http().get(TEST_URL).getForm()
+    assertNotNull(form)
+    assertEquals TEST_URL, form.base.toString()
+  }
+
+
+  void testVariables() {
+    println Http.class.metaClass.properties.collect {it.name}
+  }
+
+  void testDebug() {
+    new Http().get(TEST_URL).source.getAllElements(HTMLElementName.FORM).eachWithIndex {form, i ->
+      println "$i\t$form"
+    }
+  }
+}
---
+++ trunk/test/com/googlecode/groovyhttp/html/FormTests.groovy
@@ -0,0 +1,23 @@
+package com.googlecode.groovyhttp.html
+
+import com.googlecode.groovyhttp.Http
+
+public class FormTests extends GroovyTestCase {
+
+  void testGetForm() {
+    def form = new Http().get("http://code.google.com/p/groovy-http/").getForm()
+    assertEquals "Search projects", form.getAllElements("type", "submit", false)?.getAt(0)?.getAttributeValue("value")
+  }
+
+  void testGoogle() {
+    def anchors = new Http().get("http://search.yahoo.com/").getForm().submit(p: 'Groovy HTTP').getElement('web').getAllElements("class", "yschttl spt", false)
+    anchors.eachWithIndex {a, i ->
+      def text = a.textExtractor.toString()
+      def link = a.getAttributeValue("href").with {it.substring(it.lastIndexOf('**') + 2, it.size())}
+      //println "$i\t$text\t$link"
+    }
+    assertEquals 10, anchors.size()
+
+  }
+
+}
