b22c4ca128179ff4b3a0c36663a237a4309a956d
mingfai.ma <mingfai.ma@gmail.com>
2010-08-08 22:14:14 +0000

    fixed the findFormFields error

--- src/test/groovy/com/googlecode/groovyhttp/html/FormTests.groovy
+++ src/test/groovy/com/googlecode/groovyhttp/html/FormTests.groovy
@@ -2,15 +2,17 @@ package com.googlecode.groovyhttp.html
 
 import com.googlecode.groovyhttp.Http
 
-public class FormTests extends GroovyTestCase {
+public class FormTests extends GroovyTestCase{
 
-  void testGetForm() {
+  void testGetForm(){
     def form = new Http().get("http://code.google.com/p/groovy-http/").getForm()
     assertEquals "Search projects", form.getAllElements("type", "submit", false)?.getAt(0)?.getAttributeValue("value")
   }
 
-  void testYahooSearch() {
-    def anchors = new Http().get("http://search.yahoo.com/").getForm().submit(p: 'Groovy HTTP').getElement('web').getAllElements("class", "yschttl spt", false)
+  void testYahooSearch(){
+    def form = new Http().get("http://search.yahoo.com/").getForm()
+    println "form.element: " +form.element
+    def anchors = form.submit(p: 'Groovy HTTP').getElement('web').getAllElements("class", "yschttl spt", false)
     anchors.eachWithIndex {a, i ->
       def text = a.textExtractor.toString()
       def link = a.getAttributeValue("href").with {it.substring(it.lastIndexOf('**') + 2, it.size())}
@@ -23,7 +25,6 @@ public class FormTests extends GroovyTestCase {
   void testYahooSearchShortForm(){
     assertTrue new Http().get("http://search.yahoo.com").getForm().submit(p: 'Groovy HTTP').size() > 8000
   }
-
 
 
 }
